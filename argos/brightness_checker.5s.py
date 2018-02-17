207ae659e8d7606338df9cf8c138db97ccfc1524
strymj <yamaji.shoutarou@gmail.com>
2018-02-17 17:21:45 +0900

    update_argos_brightness_checker

--- argos/brightness_checker.5s.py
+++ argos/brightness_checker.5s.py
@@ -3,9 +3,9 @@
 import subprocess
 import re
 
-brightness_step = 0.1
+bright_step = 0.1
 gamma_default = "1.0:1.0:1.0"
-gamma_target = "1.0:0.85:0.75"
+gamma_target = "1.8:0.8:0.6"
 
 xrandr_cmd = "xrandr --verbose"
 xrandr_result = subprocess.check_output(xrandr_cmd, shell=True).split("\n")
@@ -14,36 +14,48 @@ conf = "terminal=false refresh=true"
 font = "font=Ubuntu\ Mono trim=false"
 
 disp_list = []
-brightness_list = []
+bright_list = []
 gamma_list = []
 for i in range(len(xrandr_result)) :
     split_result = xrandr_result[i].split()
     if 2 <= len(split_result) and split_result[1] == "connected" :
         disp_list.append(split_result[0])
     if 2 <= len(split_result) and split_result[0] == "Brightness:" :
-        brightness_list.append(split_result[1])
+        bright_list.append(split_result[1])
     if 2 <= len(split_result) and split_result[0] == "Gamma:" :
         gamma_list.append(split_result[1])
+
+XRANDR = "bash=xrandr\ --output\ "
+BRIGHT = "\ --brightness\ "
+GAMMA = "\ --gamma\ "
+
+def getGamma(is_redshift):
+    if is_redshift :
+        return gamma_target
+    else :
+        return gamma_default
+
+def getStatus(is_redshift) :
+    if is_redshift :
+        return "On"
+    else :
+        return "Off"
 
 
 print ":bulb:" 
 for i in range(len(disp_list)) :
-    xrandr_bright = "bash=xrandr\ --output\ " +disp_list[i] +"\ --brightness\ "
     if gamma_list[i] == gamma_default :
-        rs_status = "Off"
-        rs_switch = "On"
-        rs_target = gamma_target
+        rs = False
     else :
-        rs_status = "On"
-        rs_switch = "Off"
-        rs_target = gamma_default
-    disp_status = disp_list[i] +"   :bulb: " +brightness_list[i] +"   :computer: " +rs_status
+        rs = True
+    disp_status = disp_list[i] +"   :bulb: " +bright_list[i] +"   :computer: " +getStatus(rs)
+
     print "---"
-    print  disp_status, "|", conf, font, xrandr_bright +"1.0\ --gamma\ " +gamma_default
-    print ":bulb:     Brightness up   |", conf, font, xrandr_bright +str(float(brightness_list[i])+brightness_step)
-    print ":bulb:     Brightness down |", conf, font, xrandr_bright +str(float(brightness_list[i])-brightness_step)
-    print ":bulb:     RedShift", rs_switch, "|", conf, font, xrandr_bright +brightness_list[i] +"\ --gamma\ " +rs_target
+    print  disp_status, "|", conf, font, XRANDR +disp_list[i] +BRIGHT +"1.0" +GAMMA +gamma_default
+    print ":arrow_forward:   Brightness up   |", conf, font, XRANDR +disp_list[i] +BRIGHT +str(float(bright_list[i])+bright_step) +GAMMA +getGamma(rs)
+    print ":arrow_forward:   Brightness down |", conf, font, XRANDR +disp_list[i] +BRIGHT +str(float(bright_list[i])-bright_step) +GAMMA +getGamma(rs)
+    print ":arrow_forward:   RedShift", getStatus(not rs), "|", conf, font, XRANDR +disp_list[i] +BRIGHT +bright_list[i] +GAMMA +getGamma(not rs)
 
 # print len(disp_list), disp_list
 # print len(gamma_list), gamma_list
-# print len(brightness_list), brightness_list
+# print len(bright_list), bright_list
